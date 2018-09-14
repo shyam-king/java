8a2c54f39d750da393bc5da14f80e2fef487a9de
shyam-king <m.shyam.tnj@gmail.com>
2018-09-14 20:25:19 +0530

    New rep

---
+++ APPLET/myapp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.applet.*;
+import java.awt.*;
+
+/*
+<applet code="myapp" width=400 height=400 />
+ */
+
+public class myapp extends Applet {
+	public void paint(Graphics g)
+	{
+		g.drawString("Shyam is great", 100, 100);
+	}
+}
---
+++ Trial_Project/trial.java
@@ -0,0 +1,6 @@
+class trial{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+	}
+}
